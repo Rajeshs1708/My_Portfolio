1b57c35e60ae7724ff9f771c04d98fb77a01e8df
Rajesh <rajeshs1097@gmail.com>
2022-11-04 01:15:22 +0530

    project update

--- src/Helpers/ProjectList.js
+++ src/Helpers/ProjectList.js
@@ -1,71 +1,72 @@
-import pro1 from '../Assets/pro1.jpeg'
-import pro2 from '../Assets/whatsapp.jpg'
-import pro4 from '../Assets/dashboard.png'
-import pro5 from '../Assets/ice&fire.png'
-import pro6 from '../Assets/htmlForm.png'
-import pro7 from '../Assets/calculator.png'
+import pro1 from '../Assets/pro1.jpeg';
+import pro2 from '../Assets/whatsapp.jpg';
+import pro3 from '../Assets/youtube.png';
+import pro4 from '../Assets/dashboard.png';
+import pro5 from '../Assets/ice&fire.png';
+import pro6 from '../Assets/htmlForm.png';
+import pro7 from '../Assets/calculator.png';
 
 
 const ProjectList = [
     {
-        name:"CPM",
-        alt:"CPM",
+        name: "CPM",
+        alt: "CPM",
         image: pro1,
-        skills:"React, Node JS, Express JS, MongoDB ",
-        gitLink:"",
-        netlifyLink:""
+        skills: "React, Node JS, Express JS, MongoDB ",
+        gitLink: "",
+        netlifyLink: ""
     },
     {
-        name:"Whatsapp Clone",
-        alt:"Whatsapp",
+        name: "Whatsapp Clone",
+        alt: "Whatsapp",
         image: pro2,
-        skills:"React, Firebase",
-        gitLink:"https://github.com/Rajeshs1708/Whatsapp-clone",
-        netlifyLink:""
+        skills: "React,Material UI, Firebase",
+        gitLink: "https://github.com/Rajeshs1708/Whatsapp-clone",
+        netlifyLink: ""
     },
     {
-        name:"Facbook",
-        alt:"CPM",
-        image: pro1,
-        skills:"React, Node JS, Express JS, MongoDB ",
-        gitLink:"",
-        netlifyLink:""
+        name: "YouTube",
+        alt: "YouTube",
+        image: pro3,
+        skills: "React,Material UI",
+        gitLink: "https://github.com/Rajeshs1708/YouTube-Clone",
+        netlifyLink: "https://youtube-clone-007.netlify.app/"
     },
     {
-        name:"Admin Dashboard",
-        alt:"Admin Dashboard",
+        name: "Admin Dashboard",
+        alt: "Admin Dashboard",
         image: pro4,
-        skills:"React, Node JS, Express JS, MongoDB ",
-        gitLink:"https://github.com/Rajeshs1708/SbAdmin2-React-Task-3",
-        netlifyLink:" https://wonderful-rugelach-95ec36.netlify.app/"
+        skills: "React, Node JS, Express JS, MongoDB ",
+        gitLink: "https://github.com/Rajeshs1708/SbAdmin2-React-Task-3",
+        netlifyLink: " https://wonderful-rugelach-95ec36.netlify.app/"
     },
-    
+
     {
-        name:"API call",
-        alt:"Ice and Fire",
+        name: "API call",
+        alt: "Ice and Fire",
         image: pro5,
-        skills:"HTML, CSS, JavaScript, Bootstrap",
-        gitLink:"https://github.com/Rajeshs1708/Day-20th-Task-13-Ice-and-Fire-API",
-        netlifyLink:"https://relaxed-kangaroo-35a38d.netlify.app/"
+        skills: "HTML, CSS, JavaScript, Bootstrap",
+        gitLink: "https://github.com/Rajeshs1708/Day-20th-Task-13-Ice-and-Fire-API",
+        netlifyLink: "https://relaxed-kangaroo-35a38d.netlify.app/"
     },
     {
-        name:"HTML Form",
-        alt:"Form",
+        name: "HTML Form",
+        alt: "Form",
         image: pro6,
-        skills:"HTML, CSS, JavaScript, Bootstrap",
-        gitLink:"https://github.com/Rajeshs1708/Day-16-Task-11-HTML-Form-Table",
-        netlifyLink:" https://rainbow-medovik-b02910.netlify.app/"
+        skills: "HTML, CSS, JavaScript, Bootstrap",
+        gitLink: "https://github.com/Rajeshs1708/Day-16-Task-11-HTML-Form-Table",
+        netlifyLink: " https://rainbow-medovik-b02910.netlify.app/"
     },
     {
-        name:"Calculator",
-        alt:"Calculator",
+        name: "Calculator",
+        alt: "Calculator",
         image: pro7,
-        skills:"HTML, CSS, JavaScript, Bootstrap",
-        gitLink:"https://github.com/Rajeshs1708/Day-16-Task-11-Calculator",
-        netlifyLink:" https://papaya-horse-8c84c5.netlify.app/"
+        skills: "HTML, CSS, JavaScript, Bootstrap",
+        gitLink: "https://github.com/Rajeshs1708/Day-16-Task-11-Calculator",
+        netlifyLink: " https://papaya-horse-8c84c5.netlify.app/"
     },
-    
-    
+
+
 ]
 
 export default ProjectList;
