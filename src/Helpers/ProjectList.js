8561175e9a4e8e65219097f7ee3e18f9dc7025a8
Rajesh <rajeshs1097@gmail.com>
2023-02-28 23:42:56 +0530

    first commit

--- src/Helpers/ProjectList.js
+++ src/Helpers/ProjectList.js
@@ -15,7 +15,6 @@ import calculator from '../Assets/calculator.png'
 import oAuth from '../Assets/oAuth.png'
 
 const ProjectList = [
-  
   {
     name: 'Login System(MERN)',
     alt: 'Login System',
@@ -24,17 +23,8 @@ const ProjectList = [
     gitLink1: 'https://github.com/Rajeshs1708/Password_Reset_Flow-frontend',
     gitLink2: 'https://github.com/Rajeshs1708/Password_Reset_Flow-backend',
     netlifyLink: 'https://password-reset-flow-app.netlify.app/',
-    description:"Login MERN application is a full-stack web application that provides users with an easy way to create and manage their account and log in to a secure profile. It is built with the MERN stack (MongoDB, Express, React, Node.js) and provides users with an intuitive, secure, and user-friendly interface.It also supports secure authentication."
-  },
-  {
-    name: 'CRM(MERN)',
-    alt: 'Customer Relationship Management',
-    image: crm,
-    skills: 'React, Bootstrap, Node JS, Express JS, MongoDB ',
-    gitLink1: 'https://github.com/Rajeshs1708/CPM-webcode-frontend',
-    gitLink2: 'https://github.com/Rajeshs1708/CPM-webcode-backend',
-    netlifyLink: 'https://cpm-app-489603.netlify.app/',
-    description:"CRM MERN application is a powerful customer relationship management (CRM) solution built with MongoDB, Express, React, and Node.js. This full-stack application lets you store information about customers to help you track the status of every customer relationship. The application will be able to store and edit customer details, as well as keep notes about them"
+    description:
+      'Login MERN application is a full-stack web application that provides users with an easy way to create and manage their account and log in to a secure profile. It is built with the MERN stack (MongoDB, Express, React, Node.js) and provides users with an intuitive, secure, and user-friendly interface.It also supports secure authentication.'
   },
   {
     name: 'Blog_App(MERN)',
@@ -44,7 +34,31 @@ const ProjectList = [
     gitLink1: 'https://github.com/Rajeshs1708/Blog_app_Frontend',
     gitLink2: 'https://github.com/Rajeshs1708/Blog_app_Backend',
     netlifyLink: 'https://blog-app-001.netlify.app/',
-    description:"Blog MERN is a web application that makes it easy to create and manage content-rich blogs. It is built with the MERN (MongoDB, Express, React, Node.js) stack and is designed to be fast, secure, and user-friendly. With its simple yet modern user interface, Blog MERN provides users with an efficient way to create and manage their blogs.This blog app is a type of website where the content is presented in reverse chronological order (newer content appear first)."
+    description:
+      'Blog MERN is a web application that makes it easy to create and manage content-rich blogs. It is built with the MERN (MongoDB, Express, React, Node.js) stack and is designed to be fast, secure, and user-friendly. With its simple yet modern user interface, Blog MERN provides users with an efficient way to create and manage their blogs.This blog app is a type of website where the content is presented in reverse chronological order (newer content appear first).'
+  },
+  {
+    name: 'CRM(MERN)',
+    alt: 'Customer Relationship Management',
+    image: crm,
+    skills:
+      'React, Bootstrap, Node JS, Express JS, MongoDB, Socket.io, Razorpay ',
+    gitLink1: 'https://github.com/Rajeshs1708/CPM-webcode-frontend',
+    gitLink2: 'https://github.com/Rajeshs1708/CPM-webcode-backend',
+    netlifyLink: 'https://cpm-app-489603.netlify.app/',
+    description:
+      'CRM MERN application is a powerful customer relationship management (CRM) solution built with MongoDB, Express, React, and Node.js. This full-stack application lets you store information about customers to help you track the status of every customer relationship. The application will be able to store and edit customer details, as well as keep notes about them'
+  },
+  {
+    name: 'URL Shortener(MERN)',
+    alt: 'URL Shortener',
+    image: urlShortener,
+    skills: 'React, Node JS, Express JS, MongoDB ',
+    gitLink1: 'https://github.com/Rajeshs1708/URL-Shortener-app-frontend',
+    gitLink2: 'https://github.com/Rajeshs1708/URL-Shortener-app-backend',
+    netlifyLink: 'https://url-shortener-app-finaltask.netlify.app/',
+    description:
+      "This URL shortener application is a web application built with the MERN (MongoDB, Express, React, and Node.js) stack that is designed to shorten lengthy URLs into shorter, more manageable ones. It allows users to quickly and easily create, share, and track shortened URLs. The application can also track the number of clicks each URL has received. The idea is to minimize the web page address into something that's easier to remember and track."
   },
   {
     name: 'Login/Logout with OAuth(MERN)',
@@ -56,18 +70,10 @@ const ProjectList = [
     gitLink2:
       'https://github.com/Rajeshs1708/Login-logout-with-multiple-OAuth-backend',
     netlifyLink: 'https://login-with-oauth.netlify.app/',
-    description:"Login with multiple OAuth is an authentication system that allows users to log in to a MERN application using their existing accounts from other online service providers such as Google, Facebook, and GitHub. This authentication system uses OAuth (Open Authorization) protocol to securely authenticate users and grant access to the application. It is an efficient way for users to access the application without having to create a new account on the application itself. With this system, users can log in with just one click, streamlining the login process and making the application more user friendly and secure."
+    description:
+      'Login with multiple OAuth is an authentication system that allows users to log in to a MERN application using their existing accounts from other online service providers such as Google, Facebook, and GitHub. This authentication system uses OAuth (Open Authorization) protocol to securely authenticate users and grant access to the application. It is an efficient way for users to access the application without having to create a new account on the application itself. With this system, users can log in with just one click, streamlining the login process and making the application more user friendly and secure.'
   },
-  {
-    name: 'URL Shortener(MERN)',
-    alt: 'URL Shortener',
-    image: urlShortener,
-    skills: 'React, Node JS, Express JS, MongoDB ',
-    gitLink1: 'https://github.com/Rajeshs1708/URL-Shortener-app-frontend',
-    gitLink2: 'https://github.com/Rajeshs1708/URL-Shortener-app-backend',
-    netlifyLink: 'https://url-shortener-app-finaltask.netlify.app/',
-    description:"This URL shortener application is a web application built with the MERN (MongoDB, Express, React, and Node.js) stack that is designed to shorten lengthy URLs into shorter, more manageable ones. It allows users to quickly and easily create, share, and track shortened URLs. The application can also track the number of clicks each URL has received. The idea is to minimize the web page address into something that's easier to remember and track."
-  },
+
   {
     name: 'WhatsApp Clone(MERN)',
     alt: 'WhatsApp',
@@ -76,7 +82,8 @@ const ProjectList = [
     gitLink1: 'https://github.com/Rajeshs1708/whatsapp-mern-frontend',
     gitLink2: 'https://github.com/Rajeshs1708/whatsapp-mern-backend',
     netlifyLink: 'https://whatsapp-clone-001.netlify.app/',
-    description:"WhatsApp Clone MERN application is a full-stack web development application that allows users to develop a communication app similar to WhatsApp. This application is built using the MongoDB, Express.js, React.js, and Node.js (MERN). With this application, users can easily create a messaging platform."
+    description:
+      'WhatsApp Clone MERN application is a full-stack web development application that allows users to develop a communication app similar to WhatsApp. This application is built using the MongoDB, Express.js, React.js, and Node.js (MERN). With this application, users can easily create a messaging platform.'
   },
   {
     name: 'Amazon Clone (UI)',
@@ -86,7 +93,8 @@ const ProjectList = [
     gitLink1: 'https://github.com/Rajeshs1708/amazon-clone',
     gitLink2: 'https://github.com/Rajeshs1708/amazon-clone',
     netlifyLink: 'https://amazon-clone-123.netlify.app/',
-    description:"Amazon Clone UI is an application that replicates the look and feel of the popular Amazon website. It is designed to provide users with a seamless and easy shopping experience. The app features a modern and intuitive user interface, with easy-to-navigate menus and a wide range of product categories.The app also provides users with the ability to save their favorite items to a wishlist for later"
+    description:
+      'Amazon Clone UI is an application that replicates the look and feel of the popular Amazon website. It is designed to provide users with a seamless and easy shopping experience. The app features a modern and intuitive user interface, with easy-to-navigate menus and a wide range of product categories.The app also provides users with the ability to save their favorite items to a wishlist for later'
   },
   {
     name: 'YouTube Clone (UI)',
@@ -96,7 +104,8 @@ const ProjectList = [
     gitLink1: 'https://github.com/Rajeshs1708/YouTube-Clone',
     gitLink2: 'https://github.com/Rajeshs1708/YouTube-Clone',
     netlifyLink: 'https://youtube-clone-007.netlify.app/',
-    description:"It provides a familiar user interface that allows users to easily navigate and manage their own YouTube-like content."
+    description:
+      'It provides a familiar user interface that allows users to easily navigate and manage their own YouTube-like content.'
   },
   {
     name: 'Facebook Clone (UI)',
@@ -106,7 +115,8 @@ const ProjectList = [
     gitLink1: 'https://github.com/Rajeshs1708/facebook-clone',
     gitLink2: 'https://github.com/Rajeshs1708/facebook-clone',
     netlifyLink: 'https://facebook-clone-0001.netlify.app/',
-    description:"Facebook clone UI only application is a clone of the popular social media platform, Facebook. It offers users a clone of the user interface of the original platform, with a few minor changes"
+    description:
+      'Facebook clone UI only application is a clone of the popular social media platform, Facebook. It offers users a clone of the user interface of the original platform, with a few minor changes'
   },
   {
     name: 'Thirukkural (API call)',
@@ -116,7 +126,8 @@ const ProjectList = [
     gitLink1: 'https://github.com/Rajeshs1708/Day-20th-Task-13-Thirukkural-API',
     gitLink2: 'https://github.com/Rajeshs1708/Day-20th-Task-13-Thirukkural-API',
     netlifyLink: 'https://clever-dasik-75cb83.netlify.app/',
-    description:"This API enables users to fetch data from the Thirukkural, an ancient Tamil collection of moral proverbs. It provides access to all 1330 couplets from the Thirukkural and their English translations, as well as other detailed information such as the chapters, adhikarams, and authors. This API can be used to develop applications for learning, studying, and teaching the Thirukkural"
+    description:
+      'This API enables users to fetch data from the Thirukkural, an ancient Tamil collection of moral proverbs. It provides access to all 1330 couplets from the Thirukkural and their English translations, as well as other detailed information such as the chapters, adhikarams, and authors. This API can be used to develop applications for learning, studying, and teaching the Thirukkural'
   },
   {
     name: 'Ice and Fire (API call)',
@@ -128,7 +139,8 @@ const ProjectList = [
     gitLink2:
       'https://github.com/Rajeshs1708/Day-20th-Task-13-Ice-and-Fire-API',
     netlifyLink: 'https://relaxed-kangaroo-35a38d.netlify.app/',
-    description:"Fetch data from the Ice and Fire API is a process of retrieving data from the Ice and Fire API, an open-source database of information related to the fantasy world of George R.R. Martin's A Song of Ice and Fire series. The API provides access to a variety of data, including book and character details, location information, and more."
+    description:
+      "Fetch data from the Ice and Fire API is a process of retrieving data from the Ice and Fire API, an open-source database of information related to the fantasy world of George R.R. Martin's A Song of Ice and Fire series. The API provides access to a variety of data, including book and character details, location information, and more."
   },
   {
     name: 'Covid-19 (API call)',
@@ -138,7 +150,8 @@ const ProjectList = [
     gitLink1: ' https://github.com/Rajeshs1708/Day-20th-Task-Covid-19-API',
     gitLink2: ' https://github.com/Rajeshs1708/Day-20th-Task-Covid-19-API',
     netlifyLink: 'https://soft-selkie-b8e4fa.netlify.app/',
-    description:"Fetching data from a third-party COVID-19 API allows users to access up-to-date information about the current coronavirus pandemic. This data can be used to create detailed reports, track trends, and provide insights into the spread of the virus. It includes statistics such as the total number of confirmed cases, deaths, and recoveries, as well as detailed breakdowns by country, region, or other demographic categories. The data can also be used to generate interactive maps and charts, making it easier to visualize the impact of the virus."
+    description:
+      'Fetching data from a third-party COVID-19 API allows users to access up-to-date information about the current coronavirus pandemic. This data can be used to create detailed reports, track trends, and provide insights into the spread of the virus. It includes statistics such as the total number of confirmed cases, deaths, and recoveries, as well as detailed breakdowns by country, region, or other demographic categories. The data can also be used to generate interactive maps and charts, making it easier to visualize the impact of the virus.'
   },
   {
     name: 'Infinite Scroll',
@@ -148,7 +161,8 @@ const ProjectList = [
     gitLink1: 'https://github.com/Rajeshs1708/Infinite_Scroll',
     gitLink2: 'https://github.com/Rajeshs1708/Infinite_Scroll',
     netlifyLink: 'https://infinite-scroll-001.netlify.app/',
-    description:"Unsplash API's infinite scrolling feature allows developers to quickly and easily access large volumes of images at once. It enables developers to retrieve an unlimited number of images from the Unsplash library, without having to manually make multiple requests. The infinite scrolling feature is incredibly efficient and effective, as it enables developers to quickly and easily access high-quality images in large quantities."
+    description:
+      "Unsplash API's infinite scrolling feature allows developers to quickly and easily access large volumes of images at once. It enables developers to retrieve an unlimited number of images from the Unsplash library, without having to manually make multiple requests. The infinite scrolling feature is incredibly efficient and effective, as it enables developers to quickly and easily access high-quality images in large quantities."
   },
   {
     name: 'HTML Form',
@@ -157,7 +171,8 @@ const ProjectList = [
     skills: 'HTML, CSS, JavaScript, Bootstrap',
     gitLink1: 'https://github.com/Rajeshs1708/Day-16-Task-11-HTML-Form-Table',
     netlifyLink: ' https://rainbow-medovik-b02910.netlify.app/',
-    description:"CRUD (Create, Read, Update, Delete) is a set of operations that is used to manage data in a database. It is commonly used when developing web applications.HTML is used to create the structure of the web page, while CSS is used to style the page. JavaScript can be used to create forms and to interact with the database."
+    description:
+      'CRUD (Create, Read, Update, Delete) is a set of operations that is used to manage data in a database. It is commonly used when developing web applications.HTML is used to create the structure of the web page, while CSS is used to style the page. JavaScript can be used to create forms and to interact with the database.'
   },
   {
     name: 'Calculator',
@@ -166,7 +181,8 @@ const ProjectList = [
     skills: 'HTML, CSS, JavaScript, Bootstrap',
     gitLink1: 'https://github.com/Rajeshs1708/Day-16-Task-11-Calculator',
     netlifyLink: ' https://papaya-horse-8c84c5.netlify.app/',
-    description:"This application can be used to perform mathematical operations such as addition, subtraction, multiplication, and division. HTML is used to create the structure of the calculator, while CSS is used to style it. JavaScript can be used to capture user input, process the calculations, and display the results on the page. "
+    description:
+      'This application can be used to perform mathematical operations such as addition, subtraction, multiplication, and division. HTML is used to create the structure of the calculator, while CSS is used to style it. JavaScript can be used to capture user input, process the calculations, and display the results on the page. '
   }
 ]
 
