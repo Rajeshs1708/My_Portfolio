540107ca2b598cd83f9bfe87d0f5bf0f3fa20982
Rajesh <rajeshs1097@gmail.com>
2023-07-23 16:56:43 +0530

    first commit

--- src/Pages/Experience.js
+++ src/Pages/Experience.js
@@ -1,108 +1,108 @@
-import React from 'react'
+import React from "react";
 import {
   VerticalTimeline,
-  VerticalTimelineElement
-} from 'react-vertical-timeline-component'
-import 'react-vertical-timeline-component/style.min.css'
-import { School, Work } from '@mui/icons-material'
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { School, Work } from "@mui/icons-material";
 
-function Experience () {
+function Experience() {
   return (
-    <div className='experience' style={{ color: '#3e497a' }}>
-      <VerticalTimeline lineColor='#3e497a'>
+    <div className="experience" style={{ color: "#3e497a" }}>
+      <VerticalTimeline lineColor="#3e497a">
         <VerticalTimelineElement
-          className='vertical-timeline-element--education'
-          date='2011-2012'
-          iconStyle={{ background: '#3e497a', color: '#fff' }}
+          className="vertical-timeline-element--education"
+          date="2011-2012"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<School />}
         >
-          <figure className='text-start'>
-            <blockquote className='blockquote'>
+          <figure className="text-start">
+            <blockquote className="blockquote">
               <h4>Britannia High School</h4>
             </blockquote>
-            <figcaption className='blockquote-footer'>89.20%</figcaption>
-            <figcaption className='blockquote-footer'>Padi, Chennai</figcaption>
+            <figcaption className="blockquote-footer">89.20%</figcaption>
+            <figcaption className="blockquote-footer">Padi, Chennai</figcaption>
           </figure>
         </VerticalTimelineElement>
 
         <VerticalTimelineElement
-          className='vertical-timeline-element--education'
-          date='2012-2014'
-          iconStyle={{ background: '#3e497a', color: '#fff' }}
+          className="vertical-timeline-element--education"
+          date="2012-2014"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<School />}
         >
-          <figure className='text-start'>
-            <blockquote className='blockquote'>
+          <figure className="text-start">
+            <blockquote className="blockquote">
               <h4>Vijayanta Higher Secondary School</h4>
             </blockquote>
-            <figcaption className='blockquote-footer'>
-              91.41%(Bio Maths){' '}
+            <figcaption className="blockquote-footer">
+              91.41%(Bio Maths){" "}
             </figcaption>
-            <figcaption className='blockquote-footer'>
+            <figcaption className="blockquote-footer">
               Avadi, Chennai
             </figcaption>
           </figure>
         </VerticalTimelineElement>
 
         <VerticalTimelineElement
-          className='vertical-timeline-element--education'
-          date='2014-2018'
-          iconStyle={{ background: '#3e497a', color: '#fff' }}
+          className="vertical-timeline-element--education"
+          date="2014-2018"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<School />}
         >
-          <figure className='text-start'>
-            <blockquote className='blockquote'>
+          <figure className="text-start">
+            <blockquote className="blockquote">
               <h4>SRM Institute of Science and Technology</h4>
             </blockquote>
-            <figcaption className='blockquote-footer'>
+            <figcaption className="blockquote-footer">
               8.21 CGPA (B.Tech Civil)
             </figcaption>
-            <figcaption className='blockquote-footer'>
+            <figcaption className="blockquote-footer">
               Potheri, Kattankulathur
             </figcaption>
           </figure>
         </VerticalTimelineElement>
 
         <VerticalTimelineElement
-          className='vertical-timeline-element--education'
-          date='2022-2023'
-          iconStyle={{ background: '#3e497a', color: '#fff' }}
+          className="vertical-timeline-element--education"
+          date="2022-2023"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<School />}
         >
-          <figure className='text-start'>
-            <blockquote className='blockquote'>
+          <figure className="text-start">
+            <blockquote className="blockquote">
               <h4>
-                GUVI <span style={{ color: 'gray' }}>Zen Class</span>
+                GUVI <span style={{ color: "gray" }}>Zen Class</span>
               </h4>
             </blockquote>
-            <figcaption className='blockquote-footer'>
+            <figcaption className="blockquote-footer">
               FULL (MERN) STACK DEVELOPER
             </figcaption>
-            <figcaption className='blockquote-footer'>
+            <figcaption className="blockquote-footer">
               IIT Madras Incubated
             </figcaption>
           </figure>
         </VerticalTimelineElement>
 
         <VerticalTimelineElement
-          className='vertical-timeline-element--education'
-          date='2018-2021'
-          iconStyle={{ background: '#3e497a', color: '#fff' }}
+          className="vertical-timeline-element--education"
+          date="2018-2023"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<Work />}
         >
-          <figure className='text-start'>
-            <blockquote className='blockquote'>
+          <figure className="text-start">
+            <blockquote className="blockquote">
               <h4>Vishal Builders, Creative Construction</h4>
             </blockquote>
-            <figcaption className='blockquote-footer'>
+            <figcaption className="blockquote-footer">
               Site Supervisor
             </figcaption>
-            <figcaption className='blockquote-footer'>Full-Time</figcaption>
+            <figcaption className="blockquote-footer">Full-Time</figcaption>
           </figure>
         </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import '../Styles/Home.css'
+import React from "react";
+import "../Styles/Home.css";
 
-function Home () {
+function Home() {
   return (
     <>
-      <div className='container-fluid'>
-        <div className='about row pt-4'>
-          <div className='aboutLeft col-md-5'>
+      <div className="container-fluid">
+        <div className="about row pt-4">
+          <div className="aboutLeft col-md-5">
             <img
-              className='img-fluid mx-auto d-block'
-              src='/Images/Rajesh.jpg'
-              alt='Rajesh'
+              className="img-fluid mx-auto d-block"
+              src="/Images/Rajesh.jpg"
+              alt="Rajesh"
             />
           </div>
-          <div className='shadow text-wrap aboutRight col-md-7 p-3'>
+          <div className="shadow text-wrap aboutRight col-md-7 p-3">
             <h2>Hi,I'm Rajesh</h2>
-            <h3 data-text='Full_Stack_Developer...'>Full_Stack_Developer...</h3>
-            <div className='prompt'>
+            <h3 data-text="Full_Stack_Developer...">Full_Stack_Developer...</h3>
+            <div className="prompt">
               <p>
                 I have completed my MERN stack development course and having
                 hands-on experience in web application from creating full stack
@@ -24,21 +24,21 @@ function Home () {
                 from which i get trained technologies like React Js, Node Js,
                 MongoDB.
               </p>
-              <div className='svgIcon' style={{ width: '50px' }}>
+              <div className="svgIcon" style={{ width: "50px" }}>
                 <a
-                  rel='noreferrer'
-                  href='https://drive.google.com/file/d/1yA4aHo80fX5pTPhX0FGXpM_73CF5XeN5/view?usp=share_link'
-                  className='resume'
-                  target='_blank'
+                  rel="noreferrer"
+                  href="https://drive.google.com/file/d/1yA4aHo80fX5pTPhX0FGXpM_73CF5XeN5/view?usp=share_link"
+                  className="resume"
+                  target="_blank"
                 >
                   <img
-                    alt='Resume'
-                    src='/Images/resume.jpg'
+                    alt="Resume"
+                    src="/Images/resume.jpg"
                     style={{
-                      width: '50px',
-                      borderRadius: '50%'
+                      width: "50px",
+                      borderRadius: "50%",
                     }}
-                    title='Resume'
+                    title="Resume"
                   />
                 </a>
               </div>
@@ -47,7 +47,7 @@ function Home () {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
